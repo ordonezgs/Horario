3df185d4a3c56b5ccc1e210151ae3feebc1d7af9
Santiago Gutierrez Ordoñez <sgo_2001@hotmail.com>
2019-02-19 17:01:14 -0500

    Add files via upload

--- calculo.js
+++ calculo.js
@@ -25,6 +25,11 @@ function getDayNumber(baseDate, targetDate, daysInCycle) {
 var diact = getDayNumber(new Date(2019, 1, 18), new Date(yyyy, mm1, dd), 6)
 document.getElementById("fecha").innerHTML = hoy + " Correspondiente a día " + diact;
 document.getElementById("dia").innerHTML = diact + 1
+if (diact > 6) {
+  diasig = diact + 1
+} else {
+  diasig = diact - 5
+}
 
 switch (diact) {
   case 1:
@@ -36,7 +41,7 @@ switch (diact) {
     document.getElementById("c1l5").innerHTML = "Español"
     document.getElementById("c1l6").innerHTML = "Química"
     document.getElementById("c1l7").innerHTML = "Química"
-    document.getElementById("2").innerHTML = "Día " + (diact + 1)
+    document.getElementById("2").innerHTML = "Día " + diasig
     document.getElementById("c2l1").innerHTML = "Matematicas"
     document.getElementById("c2l2").innerHTML = "Matematicas"
     document.getElementById("c2l3").innerHTML = "Inglés"
@@ -55,7 +60,7 @@ switch (diact) {
     document.getElementById("c1l5").innerHTML = "Física"
     document.getElementById("c1l6").innerHTML = "Estadistica"
     document.getElementById("c1l7").innerHTML = "Estadistica"
-    document.getElementById("2").innerHTML = "Día " + (diacth + 1)
+    document.getElementById("2").innerHTML = "Día " + diasig
     document.getElementById("c2l1").innerHTML = "Edu. Física"
     document.getElementById("c2l2").innerHTML = "Edu. Física"
     document.getElementById("c2l3").innerHTML = "Inglés"
@@ -73,7 +78,7 @@ switch (diact) {
     document.getElementById("c1l5").innerHTML = "Química"
     document.getElementById("c1l6").innerHTML = "Artes"
     document.getElementById("c1l7").innerHTML = "Artes"
-    document.getElementById("2").innerHTML = "Día " + (diact + 1)
+    document.getElementById("2").innerHTML = "Día " + diasig
     document.getElementById("c2l1").innerHTML = "Dir. Curso"
     document.getElementById("c2l2").innerHTML = "Matematicas"
     document.getElementById("c2l3").innerHTML = "Español"
@@ -91,7 +96,7 @@ switch (diact) {
     document.getElementById("c1l5").innerHTML = "Filosofía"
     document.getElementById("c1l6").innerHTML = "Física"
     document.getElementById("c1l7").innerHTML = "Física"
-    document.getElementById("2").innerHTML = "Día " + (diact + 1)
+    document.getElementById("2").innerHTML = "Día " + diasig
     document.getElementById("c2l1").innerHTML = "Inglés"
     document.getElementById("c2l2").innerHTML = "Inglés"
     document.getElementById("c2l3").innerHTML = "Religión"
@@ -109,7 +114,7 @@ switch (diact) {
     document.getElementById("c1l5").innerHTML = "Informatica"
     document.getElementById("c1l6").innerHTML = "Química"
     document.getElementById("c1l7").innerHTML = "Química"
-    document.getElementById("2").innerHTML = "Día " + (diact + 1)
+    document.getElementById("2").innerHTML = "Día " + diasig
     document.getElementById("c2l1").innerHTML = "Ética"
     document.getElementById("c2l2").innerHTML = "Español"
     document.getElementById("c2l3").innerHTML = "Filosofía"
@@ -127,7 +132,7 @@ switch (diact) {
     document.getElementById("c1l5").innerHTML = "Matematicas"
     document.getElementById("c1l6").innerHTML = "Física"
     document.getElementById("c1l7").innerHTML = "Historia de Colombia"
-    document.getElementById("2").innerHTML = "Día " + (diact + 1)
+    document.getElementById("2").innerHTML = "Día " + diasig
     document.getElementById("c2l1").innerHTML = "Inglés"
     document.getElementById("c2l2").innerHTML = "Inglés"
     document.getElementById("c2l3").innerHTML = "Investigación"
