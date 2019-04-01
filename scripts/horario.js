9773c75e74fc58bc7f626f32f75a287a1c7b0c5a
Santiago G. Ordoñez <sgo_2001@hotmail.com>
2019-03-31 23:06:08 -0500

    Update horario.js

--- scripts/horario.js
+++ scripts/horario.js
@@ -20,8 +20,8 @@ var d2 ={
 var d3 ={
   h1: "Edu. Física",
   h2: "Edu. Física",
-  h3: "Estadistica/<br>Geometria",
-  h4: "Estadistica/<br>Geometria",
+  h3: "Estadistica",
+  h4: "Estadistica",
   h5: "Física",
   h6: "Inglés",
   h7: "Inglés"
@@ -36,8 +36,8 @@ var d4 ={
   h7: "Inglés"
 }
 var d5 ={
-  h1: "Sociales/<br>Catedra",
-  h2: "Sociales/<br>Catedra",
+  h1: "Sociales",
+  h2: "Sociales",
   h3: "Música",
   h4: "Música",
   h5: "Filosofía",
@@ -46,7 +46,7 @@ var d5 ={
 }
 var d6 ={
   h1: "Español",
-  h2: "Historia de Colombia",
+  h2: "Hist. de Colombia",
   h3: "Filosofía",
   h4: "Etica",
   h5: "Química",
