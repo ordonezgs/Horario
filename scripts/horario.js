048f5dcb34a975d5f3d20d8b164dd95392470c87
Santiago G. Ordoñez <sgo_2001@hotmail.com>
2019-04-11 19:10:36 -0500

    Update horario.js

--- scripts/horario.js
+++ scripts/horario.js
@@ -6,7 +6,8 @@ var d1 ={
   h4: "Informatica",
   h5: "Informatica",
   h6: "Química",
-  h7: "Biología"
+  h7: "Biología",
+  u: "Diario"
 }
 var d2 ={
   h1: "Matematicas",
@@ -15,7 +16,8 @@ var d2 ={
   h4: "Español",
   h5: "Física",
   h6: "Química",
-  h7: "Química"
+  h7: "Química",
+  u: "Diario"
 }
 var d3 ={
   h1: "Edu. Física",
@@ -24,7 +26,8 @@ var d3 ={
   h4: "Estadistica",
   h5: "Física",
   h6: "Inglés",
-  h7: "Inglés"
+  h7: "Inglés",
+  u: "Edu. Física"
 }
 var d4 ={
   h1: "Dir. Curso",
@@ -33,7 +36,8 @@ var d4 ={
   h4: "Física",
   h5: "Física",
   h6: "Inglés",
-  h7: "Inglés"
+  h7: "Inglés",
+  u: "Diario"
 }
 var d5 ={
   h1: "Sociales",
@@ -42,7 +46,8 @@ var d5 ={
   h4: "Música",
   h5: "Filosofía",
   h6: "Matematicas",
-  h7: "Matematicas"
+  h7: "Matematicas",
+  u: "Diario"
 }
 var d6 ={
   h1: "Español",
@@ -51,7 +56,8 @@ var d6 ={
   h4: "Etica",
   h5: "Química",
   h6: "Inglés",
-  h7: "Inglés"
+  h7: "Inglés",
+  u: "Edu. Física"
 }
 //Definir Función
 
@@ -64,6 +70,7 @@ function cargarHorario() {
   document.getElementById("c1l5").innerHTML = d1.h5
   document.getElementById("c1l6").innerHTML = d1.h6
   document.getElementById("c1l7").innerHTML = d1.h7
+  document.getElementById("u1").innerHTML = d1.u
   //Dia 2
   document.getElementById("c2l1").innerHTML = d2.h1
   document.getElementById("c2l2").innerHTML = d2.h2
@@ -72,6 +79,7 @@ function cargarHorario() {
   document.getElementById("c2l5").innerHTML = d2.h5
   document.getElementById("c2l6").innerHTML = d2.h6
   document.getElementById("c2l7").innerHTML = d2.h7
+  document.getElementById("u2").innerHTML = d2.u
   //Dia 3
   document.getElementById("c3l1").innerHTML = d3.h1
   document.getElementById("c3l2").innerHTML = d3.h2
@@ -80,6 +88,7 @@ function cargarHorario() {
   document.getElementById("c3l5").innerHTML = d3.h5
   document.getElementById("c3l6").innerHTML = d3.h6
   document.getElementById("c3l7").innerHTML = d3.h7
+  document.getElementById("u3").innerHTML = d3.u
   //Dia 4
   document.getElementById("c4l1").innerHTML = d4.h1
   document.getElementById("c4l2").innerHTML = d4.h2
@@ -88,6 +97,7 @@ function cargarHorario() {
   document.getElementById("c4l5").innerHTML = d4.h5
   document.getElementById("c4l6").innerHTML = d4.h6
   document.getElementById("c4l7").innerHTML = d4.h7
+  document.getElementById("u4").innerHTML = d4.u
   //Dia 5
   document.getElementById("c5l1").innerHTML = d5.h1
   document.getElementById("c5l2").innerHTML = d5.h2
@@ -96,6 +106,7 @@ function cargarHorario() {
   document.getElementById("c5l5").innerHTML = d5.h5
   document.getElementById("c5l6").innerHTML = d5.h6
   document.getElementById("c5l7").innerHTML = d5.h7
+  document.getElementById("u5").innerHTML = d5.u
   //Dia 6
   document.getElementById("c6l1").innerHTML = d6.h1
   document.getElementById("c6l2").innerHTML = d6.h2
@@ -104,6 +115,7 @@ function cargarHorario() {
   document.getElementById("c6l5").innerHTML = d6.h5
   document.getElementById("c6l6").innerHTML = d6.h6
   document.getElementById("c6l7").innerHTML = d6.h7
+  document.getElementById("u6").innerHTML = d6.u
   //
   return;
 }
